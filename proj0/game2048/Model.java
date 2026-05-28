54dfca9198327ea71959fcc9bdb5155cc22c41db
24039100046 <24039100046@stu.xidian.edu.cn>
2026-05-28 21:28:05 +0800

    emptySpaceExists方法

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -137,8 +137,16 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
-        return false;
+        boolean exist = false;
+        int size = b.size();
+        for (int col = 0; col < size; col++) {
+            for (int row = 0; row < size; row++) {
+                if (b.tile(col, row) == null) {
+                    exist = true;
+                }
+            }
+        }
+        return exist;
     }
 
     /**
